b9ce46e49a3814cecb4d4c53dba83790741b47b8
kamyarkamali <kamyarkamali021@gmail.com>
2024-07-08 03:29:31 +0330

    created slider and footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,9 +5,12 @@ import { IoLogoInstagram } from "react-icons/io5";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa6";
 
+//imagelogo
+import logo from "../assets/images/logo3.png";
+
 function Footer() {
   return (
-    <div className="max-w-[1417px] h-[291px] bg-[#E00A0A] rounded-t-2xl">
+    <div className="max-w-[1417px] lg:h-[291px] bg-[#E00A0A] rounded-t-2xl">
       <div className="border-b-[6px] flex flex-col md:flex-row items-center justify-evenly max-w-[1417px] h-[78px]">
         {/* sochial medi icons */}
         <div className="text-white flex justify-between items-center w-[300px]">
@@ -45,11 +48,48 @@ function Footer() {
           </div>
         </div>
 
-        {/* <div>2</div> */}
-
         <div className="flex items-center gap-1 text-white text-[13px]">
           <p>آدرس:</p>
           <p>آدرس مورد نظر قرار داده شود</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row justify-evenly items-center p-5">
+        <div className="flex flex-col lg:flex-row items-center lg:gap-[8rem] gap-[4rem]">
+          <div className="text-[12px] font-bold flex flex-col gap-4 items-start text-white">
+            <span>دسترسی اسان</span>
+            <span>وبلاگ</span>
+            <span>خدمات ما</span>
+            <span>درباره ما</span>
+            <span>مسیریابی</span>
+          </div>
+
+          <div className="text-[12px] font-bold flex flex-col gap-4 items-start text-white">
+            <span>تعمیرات تخصصی</span>
+            <span>BENZ</span>
+            <span>BMW</span>
+            <span>Rangerover</span>
+            <span>Porsche</span>
+          </div>
+
+          <div className="text-[12px] font-bold flex flex-col gap-4 lg:items-start items-center text-white">
+            <span>ارتباط با ما:</span>
+            <span>نام مورد نظر</span>
+            <span>شماره تماس نوشته شود</span>
+          </div>
+        </div>
+
+        <div>
+          <img src={logo} alt="" />
+          <div className="text-white flex flex-col  items-center mt-3">
+            <p>تعمیرگاه تخصصی اتوموبیل های آلمانی</p>
+            <a href="mailto:ایمیل نوشته شود"> ایمیل نوشته شود </a>
+            <div className="flex  items-center gap-2">
+              <IoLogoInstagram />
+              <FaTelegramPlane />
+              <FaWhatsapp />
+            </div>
+          </div>
         </div>
       </div>
     </div>
